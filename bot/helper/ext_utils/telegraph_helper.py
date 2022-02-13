fec2f1995340f458f22b462a53d965523d223f0b
TPL-Master <96399067+TPL-Master@users.noreply.github.com>
2022-02-13 19:12:54 +0530

    Gv

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror bot', 'https://github.com/anime-republic/MIRROR-HUNTER')
+telegraph=TelegraphHelper('Prime BotZ', 'https://t.me//Prime_BotZ')
